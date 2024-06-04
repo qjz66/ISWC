c9fc76dcd0b75fba0aa665df19bc34df17af2088
Ling521314 <2831709830@qq.com>
2024-06-04 20:23:36 +0800

    添加团队信息

---
+++ front-end/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer-info">明密文Team 联系我们:2199921383@qq.com</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.footer-info {
+  font-size: 20px;
+  color: white;
+  text-align: center;
+  padding-bottom: 20px;
+}
+</style>
